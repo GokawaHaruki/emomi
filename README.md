ca895863a7a392efba93e02da1f621266a483fa7
arrowrowe <arrowrowe@gmail.com>
2016-04-09 16:39:28 +0800

    Format Output, Support Copy and Random

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# emomi
+# Emomi
+
 Yet another emoji helper.
+
+## Install
+
+```
+$ npm install -g emomi
+```
+
+## Usage
+
+```
+$ emomi -h
+
+Yet another emoji helper.
+
+  Usage
+  $ emomi [keywords]
+
+  Options
+  -n, --copy-name   Copy a most likely emoji's name, if found.
+  -e, --copy-emoji  Copy a most likely emoji, if found.
+  -l, --repl        Start a REPL session.
+  -h, --help        Display this help.
+  -v, --version     Display the current version.
+  -d, --debug       Show debug info.
+
+Examples
+  $ emomi happy -n  # Copy the first search result of 'happy'.
+  $ emomi -e        # Copy a random emoji.
+  $ emomi me -d     # Sometimes emojis cause blank output. Go debug mode.
+```
--- bin/emomi.js
+++ bin/emomi.js
@@ -1,19 +1,29 @@
 #!/usr/bin/env node
 
+'use strict';
+
 const cli = require('meow')(`
   Usage
   $ emomi [keywords]
 
   Options
-  -n, --copy-name   Copy emoji's name, if exactly one found.
-  -e, --copy-emoji  Copy emoji, if exactly one found.
+  -n, --copy-name   Copy a most likely emoji's name, if found.
+  -e, --copy-emoji  Copy a most likely emoji, if found.
   -l, --repl        Start a REPL session.
+  -h, --help        Display this help.
+  -v, --version     Display the current version.
+  -d, --debug       Show debug info.
 
 Examples
-  $ emomi grinning -n
+  $ emomi happy -n  # Copy the first search result of 'happy'.
+  $ emomi -e        # Copy a random emoji.
+  $ emomi me -d     # Sometimes emojis cause blank output. Go debug mode.
 
 `, {
   alias: {
+    h: 'help',
+    v: 'version',
+    d: 'debug',
     n: 'copy-name',
     e: 'copy-emoji',
     l: 'repl'
@@ -22,23 +32,56 @@ Examples
 
 const emomi = require('../index');
 
-const DEFAULT_PAD_CHAR = ' ';
-const leftPad = (length, text, char) => text.length >= length ? text : ((char || DEFAULT_PAD_CHAR).repeat(length - text.length) + text);
+const displayOne = (emoji) => ':' + emoji.name + ': ' + emoji.char;
 
-const DEFAULT_EACH_LINE = 4;
-const separate = (array, width) => {
-  const matrix = [];
-  width = width || DEFAULT_EACH_LINE;
-  while (array.length) {
-    matrix.push(array.splice(0, width));
-  }
-  return matrix;
+const stringWidth = require('string-width');
+
+const WIDTH_EACH_EMOJI = 12;
+const WIDTH_PADDING = 2;
+const WIDTH_MARGIN = 4;
+const EMOJI_COUNT_EACH_LINE = 4;
+
+const TEXT_PADDING = ' '.repeat(WIDTH_PADDING);
+const WIDTH_EACH_TOTAL = WIDTH_EACH_EMOJI + WIDTH_PADDING + WIDTH_MARGIN;
+const WIDTH_LINE = WIDTH_EACH_TOTAL * EMOJI_COUNT_EACH_LINE;
+const displayAsMatrix = (emojis) => {
+  const rows = [];
+  let row = '';
+  emojis.forEach((emoji) => {
+    const one = displayOne(emoji);
+    if (stringWidth(row + one) >= WIDTH_LINE) {
+      rows.push(row);
+      row = '';
+    } else {
+      row += one + TEXT_PADDING;
+      row += ' '.repeat(WIDTH_EACH_TOTAL - (stringWidth(row) % WIDTH_EACH_TOTAL));
+    }
+  });
+  return rows.join('\n');
 };
 
-const DEFAULT_EACH_WIDTH = 20;
-const DEFAULT_MARGIN = 4;
-const displayAsLine = (emojis) => emojis.map((emoji) => leftPad(DEFAULT_EACH_WIDTH, ':' + emoji.name + ': ' + emoji.char)).join(' '.repeat(DEFAULT_MARGIN));
-const displayAsMatrix = (emojis) => separate(emojis).map(displayAsLine).join('\n');
+const Promise = require('bluebird');
+const copyPaste = require('copy-paste');
+const copy = Promise.promisify(copyPaste.copy);
+const copyEmoji = (emoji) =>
+  cli.flags.e ? copy(emoji.char) :
+  cli.flags.n ? copy(emoji.name) :
+  Promise.resolve(null);
+
+const display = (out) => (emojis) => {
+  if (cli.flags.d) {
+    out.log(emojis);
+  }
+  if (emojis.length) {
+    out.log(displayAsMatrix(emojis));
+    return copyEmoji(emojis[0]);
+  } else {
+    out.log('No matching emoji.');
+  }
+};
+
+const exit = () => process.exit(0);
+const rand = (array) => array[Math.floor(Math.random() * array.length)];
 
 if (cli.flags.l) {
   const vorpal = require('vorpal')();
@@ -46,14 +89,18 @@ if (cli.flags.l) {
     .command('search <keywords...>', 'Search by keywords.')
     .action(function (args) {
       return emomi.search(args.keywords)
-        .then(displayAsMatrix)
-        .then(this.log.bind(this));
+        .then(display(this));
     });
   vorpal
     .delimiter(cli.pkg.name + ' $')
     .show();
 } else if (cli.input.length) {
   emomi.search(cli.input)
-    .then(displayAsMatrix)
-    .then(console.log.bind(console));
+    .then(display(console))
+    .then(exit);
+} else {
+  const emoji = rand(emomi.emojis);
+  console.log(displayOne(emoji));
+  copyEmoji(emoji)
+    .then(exit);
 }
